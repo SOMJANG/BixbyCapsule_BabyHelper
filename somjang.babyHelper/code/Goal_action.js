d7a4887c041d56025eaf8c4707b3006ed1b28fca
DonghyunJang <jjdh11117@gmail.com>
2019-10-24 17:37:54 +0900

    edit capsule

--- somjang.babyHelper/code/Goal_action.js
+++ somjang.babyHelper/code/Goal_action.js
@@ -1,29 +1,80 @@
-module.exports.function = function goal_action (food, human, goal_flag) {
+module.exports.function = function goal_action (food, age, ingredient, goal_flag) {
 
 var Result = "나는 기본값이에요.";
 
-if(goal_flag == "recipe") {
-  if(food == "쌀미음") {
-    Result = "물에 쌀을 불리고 믹서기에 갈아준 뒤 약불로 끓인다"
+
+if (goal_flag == "eat_possible") {
+
+  if (food == "이유식") {
+    Result = "이유식은 5개월부터 먹어요"
+  }
+  else if (food == "쌀미음") {
+    Result = "쌀미음은 5개월 부터 먹을 수 있어요."
+  }
+  else if (food == "밥") {
+    Result = "10개월 이상부터 먹는 게 좋아요."
+  }
+  if (ingredient == "쌀" || ingredient == "감자") {
+    Result = "5개월 이상부터 먹을 수 있어요."
+  }
+  else if (ingredient == "소고기" || ingredient == "당근") {
+    Result = "6개월 이상부터 먹을 수 있어요."
+  }
+  else if (ingredient == "시금치" || ingredient == "미역") {
+    Result = "8개월 이상부터 먹을 수 있어요."
   }
 
-  if(food == "소고기 당근 무른 밥") {
-    Result = "소고기를 삶고 당근을 자르고 같이 끓인다"
+  if (age < 5) {
+      Result = "아직 모유수유 할 때입니다."
   }
-}
 
-if(goal_flag == "eat_possible") {
-  if(food == "쌀미음") {
-    if(human == "아기") {
-      Result = "먹으면 죽어요"
+  else if (age > 4 && age < 7) {
+    if (food == "이유식" || food == "쌀미음" || food == "감자미음") {
+      Result = food + "먹을 수 있어요"
     }
-    if(human == "아저씨") {
-      Result = "먹어도 돼요"
+    else {
+      Result = '아직은 먹을 수 없어요.'
     }
-    
-    if(human == "아줌마") {
-      Result = "아줌마는 먹어도 괜찮아요"
+    if (ingredient == "감자" || ingredient == "쌀") {
+      Result = "먹을 수 있어요"
     }
+    else {
+      Result = "아직은 먹을 수 없어요"
+    }
+  }
+
+  else if (age > 6 && age < 9) {
+    if (food == "이유식" || food == "소고기이유식" || food == "당근이유식") {
+      Result = "먹을 수 있어요"
+    }
+    else {
+      Result = '아직은 먹을 수 없어요.'
+    }
+    if (ingredient == "소고기" || ingredient == "당근") {
+      Result = "먹을 수 있어요"
+    }
+    else {
+      Result = "아직은 먹을 수 없어요"
+    }
+  }
+
+  else if (age > 7 && age < 11) {
+    if (food == "이유식" || food == "시금치스프" || food == "미역국") {
+      Result = "먹을 수 있어요"
+    }
+    else {
+      Result = '아직은 먹을 수 없어요.'
+    }
+    if (ingredient == "시금치" || ingredient == "미역") {
+      Result = "먹을 수 있어요"
+    }
+    else {
+      Result = "아직은 먹을 수 없어요"
+    }
+  }
+
+  else if (age > 9) {
+    Resut = "이제는 밥도 잘 먹을 수 있는 나이가 되었어요"
   }
 }
   return Result;
